5f90cbc5d6ce7d6495b6e4002662f0388a36ec2a
borecnejvetsi <mhrvol@seznam.cz>
2016-05-27 01:09:16 +0200

    init test

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/skytzi/Desktop/pyfb/pyfacebook-master/examples/fbsample/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
